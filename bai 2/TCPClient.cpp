9b9721b4cdb8341ab93b009f0c9ef4ba622467b5
nhanvu0901 <75715771+nhanvu0901@users.noreply.github.com>
2022-05-15 23:19:11 +0700

    Add files via upload

---
+++ bai 2/TCPClient.cpp
@@ -0,0 +1,59 @@
+﻿// Tao client nhan file tu server.
+//
+
+#include <stdio.h>
+#include <WinSock2.h>
+
+#pragma comment(lib, "ws2_32")
+#pragma warning(disable:4996)
+
+int main()
+{
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    SOCKET client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    SOCKADDR_IN addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(8000);
+
+    system("pause");
+
+    int ret = connect(client, (SOCKADDR*)&addr, sizeof(addr));
+    if (ret == SOCKET_ERROR)
+    {
+        ret = WSAGetLastError();
+        printf("Ket noi khong thanh cong - %d\n", ret);
+        return 1;
+    }
+
+
+
+    char buf[2048];
+
+    ret = recv(client, buf, sizeof(buf), 0);
+    buf[ret] = 0;
+    printf("%s\n", buf);
+
+
+    char input[256];
+    while (1) {
+        printf("Nhap xau ky tu: ");
+        fgets(input, sizeof(input), stdin);
+        if (strncmp(input, "exit", 4) == 0)
+            break;
+
+        send(client, input, strlen(input), 0);
+
+    }
+
+
+
+
+
+
+    closesocket(client);
+    WSACleanup();
+}
---
+++ bai 2/TCPServer.cpp
@@ -0,0 +1,57 @@
+﻿
+
+
+
+// MultithreadServer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <winsock2.h>
+#include <stdio.h>
+#pragma comment(lib, "ws2_32")
+DWORD WINAPI ClientThread(LPVOID lpParam)
+{
+    ​
+        SOCKET client = *(SOCKET*)lpParam;
+    int ret;
+    char buf[256];
+    // Nhan du lieu tu client va in ra man hinh
+    while (1)
+    {
+        ​
+            ret = recv(client, buf, sizeof(buf), 0);
+        if (ret <= 0)
+            break;
+        buf[ret] = 0;
+        printf("Du lieu nhan duoc: %s\n", buf);
+    }​
+        closesocket(client);
+}​
+int main()
+{
+    ​
+        // Khoi tao thu vien
+        WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+    // Tao socket
+    SOCKET listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    // Khai bao dia chi server
+    SOCKADDR_IN addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(8000);
+    // Gan cau truc dia chi voi socket
+    bind(listener, (SOCKADDR*)&addr, sizeof(addr));
+    // Chuyen sang trang thai cho ket noi
+    listen(listener, 5);
+    while (1)
+    {
+        ​
+            SOCKET client = accept(listener, NULL, NULL);
+        printf("Client moi ket noi: %d\n", client);
+        CreateThread(0, 0, ClientThread, &client, 0, 0);
+    }​
+}​
+
+
+
+
+
